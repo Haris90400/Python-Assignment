12669708570bda2e386fdcad85160aab1f056943
Khan Haris <112855603+Haris90400@users.noreply.github.com>
2022-10-15 02:21:52 +0530

    Create Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,27 @@
+C =([[1, 2, 3, 4, 5], 
+ [6, 7, 8, 9, 10], 
+ [11, 12, 13, -14, 15], 
+ [16, 17, 18, 19, 20], 
+ [2, 2, 2, 2, 2]]) 
+
+list =[] 
+#ASSINMENT_1
+1.for x in C:
+    for number in x:
+        if number == 7:
+            list.append(number)
+        elif number== 8:
+            list.append(number)
+print(list)
+2. for x in C:
+    for number in x:
+        if number == 1:
+            list.append(number)
+        elif number== 6:
+            list.append(number)
+        elif number== 11:
+            list.append(number)
+        elif number== 16:
+            list.append(number)
+print(list)
+3. 
