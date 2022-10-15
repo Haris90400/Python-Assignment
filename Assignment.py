9e6d11f803fdadff1cafb43b853b0798a8fe5aa8
Khan Haris <112855603+Haris90400@users.noreply.github.com>
2022-10-15 14:27:20 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -4,36 +4,33 @@ C =([[1, 2, 3, 4, 5],
  [16, 17, 18, 19, 20], 
  [2, 2, 2, 2, 2]]) 
 
-list =[] 
+
 #ASSINMENT_1
 1.for x in C:
     for number in x:
         if number == 7:
-            list.append(number)
+            print(number)
         elif number== 8:
-            list.append(number)
-print(list)
-2. for x in C:
+            print(number)
+
+2.for x in C:
     for number in x:
         if number == 1:
-            list.append(number)
+            print(number)
         elif number== 6:
-            list.append(number)
+            print(number)
         elif number== 11:
-            list.append(number)
+            print(number)
         elif number== 16:
-            list.append(number)
-print(list)
-3. C = ([[1, 2, 3, 4, 5], 
- [6, 7, 8, 9, 10], 
- [11, 12, 13, -14, 15], 
- [16, 17, 18, 19, 20], 
- [2, 2, 2, 2, 2]]) 
-list=[]
+            print(number)
+
+3. 
 for _ in C:
-    list.append(C[1][0])
-    list.append(C[2][0])
-    list.append(C[3][0])
-    list.append(C[4][0])
+    print(C[1][0])
+    print(C[2][0])
+    print(C[3][0])
+    print(C[4][0])
     break
-print(list)
+
+
+
