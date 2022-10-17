f3e2059058a1ccf0a02bb68f2d687c3e2bd91b1e
Khan Haris <112855603+Haris90400@users.noreply.github.com>
2022-10-17 13:02:04 -0700

    Create Assignment4.py

---
+++ Assignment4.py
@@ -0,0 +1,18 @@
+#Assignment-1
+
+student = {'name':'raj', 'age':16, 'gender':'male', 'class':10}
+
+for key,value in student.items():
+    print(key,value)
+    
+
+#Assignment-2
+
+1. d = {1:'a', 2:'b'}
+
+print(d.get(2))
+
+2. d = {1:'a', 2:'b'}
+
+d.pop(2)
+print(d)
