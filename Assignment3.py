a4453853fbf02ff681dabb1e91fe70efbe7bbd45
Khan Haris <112855603+Haris90400@users.noreply.github.com>
2022-10-17 12:37:59 -0700

    Create Assignment3.py

---
+++ Assignment3.py
@@ -0,0 +1,15 @@
+
+#Assignment1
+
+#Assignment2
+for i in range(1,10):
+    if i%2==0:
+        print(i*i)
+        
+        
+#Assignment3
+for number in range(1,4):
+    if number==1:
+        print(number)
+    else:
+        break
