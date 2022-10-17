bc7d631fd94c72b6ce54dcbb8265d4a9e899694b
Khan Haris <112855603+Haris90400@users.noreply.github.com>
2022-10-17 12:08:26 -0700

    Create Assignment2.py

---
+++ Assignment2.py
@@ -0,0 +1,15 @@
+lst_1=[1,7,5,3,4]
+
+#Assignment-1
+for number in lst_1:
+    if number ==3:
+        print("Number is present")
+
+for number in lst_1:
+    lst_1[2]=21
+    
+print(lst_1)
+
+#Assignment-2
+string="Elizabeth is queen of England"
+print(string[:18])
